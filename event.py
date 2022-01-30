b5237143dbf523d6a652d8cf79ee4b56ae25a04f
ClimberMel <mel.pryor@shaw.ca>
2022-01-30 14:00:12 -0800

    add place holders for remaining events

--- event.py
+++ event.py
@@ -72,6 +72,19 @@ You glance at Ruth where she has squirreled herself behind
 her mother’s leg. Her eyes implore you to silence.
 '''
 
+e4 = ''''''
+
+e5 = ''''''
+
+e6 = ''''''
+
+e7 = ''''''
+
+e8 = '''The driver smokes and watches as you drag your cases to the 
+back of the motor coach. The rack is set inconveniently high on 
+the vehicle. You get a grip on the heavier case.
+'''
+
 e9 = '''You crouch down and ask Ruth what she meant.
 “It’s scary at the festival,” Ruth says. “It’s bright and hot and 
 the flames go all over!” Her face and voice are both childlike. 
@@ -80,6 +93,523 @@ unfathomable things.
 May rolls her eyes and tousles her daughter’s hair. Ruth looks 
 at the ground.
 '''
+
+e10 = ''''''
+
+e11 = ''''''
+
+e12 = ''''''
+
+e13 = ''''''
+
+e14 = ''''''
+
+e15 = ''''''
+
+e16 = ''''''
+
+e17 = ''''''
+
+e18 = ''''''
+
+e19 = ''''''
+
+e20 = ''''''
+
+e21 = ''''''
+
+e22 = ''''''
+
+e23 = ''''''
+
+e24 = ''''''
+
+e25 = ''''''
+
+e26 = ''''''
+
+e27 = ''''''
+
+e28 = ''''''
+
+e29 = ''''''
+
+e30 = ''''''
+
+e31 = ''''''
+
+e32 = ''''''
+
+e33 = ''''''
+
+e34 = ''''''
+
+e35 = ''''''
+
+e36 = ''''''
+
+e37 = ''''''
+
+e38 = '''The driver continues to enjoy his cigarette, watching with keen 
+interest as you struggle with the cases. You grit your teeth and 
+heave the second one into place. Perhaps the residents of Arkham 
+will have better manners.
+'''
+
+e39 = ''''''
+
+e40 = ''''''
+
+e41 = ''''''
+
+e42 = ''''''
+
+e43 = ''''''
+
+e44 = ''''''
+
+e45 = ''''''
+
+e46 = ''''''
+
+e47 = ''''''
+
+e48 = ''''''
+
+e49 = ''''''
+
+e50 = '''''' 
+
+e51 = '''''' 
+
+e52 = '''''' 
+
+e53 = '''''' 
+
+e54 = '''''' 
+
+e55 = '''''' 
+
+e56 = '''''' 
+
+e57 = '''''' 
+
+e58 = '''''' 
+
+e59 = '''''' 
+
+e60 = '''''' 
+
+e61 = '''''' 
+
+e62 = '''''' 
+
+e63 = '''''' 
+
+e64 = '''''' 
+
+e65 = '''''' 
+
+e66 = '''''' 
+
+e67 = '''''' 
+
+e68 = '''''' 
+
+e69 = '''''' 
+
+e70 = '''''' 
+
+e71 = '''''' 
+
+e72 = '''''' 
+
+e73 = '''''' 
+
+e74 = '''''' 
+
+e75 = '''''' 
+
+e76 = '''''' 
+
+e77 = '''''' 
+
+e78 = '''''' 
+
+e79 = '''''' 
+
+e80 = '''''' 
+
+e81 = '''''' 
+
+e82 = '''''' 
+
+e83 = '''''' 
+
+e84 = '''''' 
+
+e85 = '''''' 
+
+e86 = '''''' 
+
+e87 = '''''' 
+
+e88 = '''''' 
+
+e89 = '''''' 
+
+e90 = '''''' 
+
+e91 = '''''' 
+
+e92 = '''''' 
+
+e93 = '''''' 
+
+e94 = '''''' 
+
+e95 = '''''' 
+
+e96 = '''''' 
+
+e97 = '''''' 
+
+e98 = '''''' 
+
+e99 = '''''' 
+
+e100 = '''''' 
+
+e101 = '''''' 
+
+e102 = '''''' 
+
+e103 = '''''' 
+
+e104 = '''''' 
+
+e105 = '''''' 
+
+e106 = '''''' 
+
+e107 = '''''' 
+
+e108 = '''''' 
+
+e109 = '''''' 
+
+e110 = '''''' 
+
+e111 = '''''' 
+
+e112 = '''''' 
+
+e113 = '''''' 
+
+e114 = '''''' 
+
+e115 = '''''' 
+
+e116 = '''''' 
+
+e117 = '''''' 
+
+e118 = '''''' 
+
+e119 = '''''' 
+
+e120 = '''''' 
+
+e121 = '''''' 
+
+e122 = '''''' 
+
+e123 = '''''' 
+
+e124 = '''''' 
+
+e125 = '''''' 
+
+e126 = '''''' 
+
+e127 = '''''' 
+
+e128 = '''''' 
+
+e129 = '''''' 
+
+e130 = '''''' 
+
+e131 = '''''' 
+
+e132 = '''''' 
+
+e133 = '''''' 
+
+e134 = '''''' 
+
+e135 = '''''' 
+
+e136 = '''''' 
+
+e137 = '''''' 
+
+e138 = '''''' 
+
+e139 = '''''' 
+
+e140 = '''''' 
+
+e141 = '''''' 
+
+e142 = '''''' 
+
+e143 = '''''' 
+
+e144 = '''''' 
+
+e145 = '''''' 
+
+e146 = '''''' 
+
+e147 = '''''' 
+
+e148 = '''''' 
+
+e149 = '''''' 
+
+e150 = '''''' 
+
+e151 = '''''' 
+
+e152 = '''''' 
+
+e153 = '''''' 
+
+e154 = '''''' 
+
+e155 = '''''' 
+
+e156 = '''''' 
+
+e157 = '''''' 
+
+e158 = '''''' 
+
+e159 = '''''' 
+
+e160 = '''''' 
+
+e161 = '''''' 
+
+e162 = '''''' 
+
+e163 = '''''' 
+
+e164 = '''''' 
+
+e165 = '''''' 
+
+e166 = '''''' 
+
+e167 = '''''' 
+
+e168 = '''''' 
+
+e169 = '''''' 
+
+e170 = '''''' 
+
+e171 = '''''' 
+
+e172 = '''''' 
+
+e173 = '''''' 
+
+e174 = '''''' 
+
+e175 = '''''' 
+
+e176 = '''''' 
+
+e177 = '''''' 
+
+e178 = '''''' 
+
+e179 = '''''' 
+
+e180 = '''''' 
+
+e181 = '''''' 
+
+e182 = '''''' 
+
+e183 = '''''' 
+
+e184 = '''''' 
+
+e185 = '''''' 
+
+e186 = '''''' 
+
+e187 = '''''' 
+
+e188 = '''''' 
+
+e189 = '''''' 
+
+e190 = '''''' 
+
+e191 = '''''' 
+
+e192 = '''''' 
+
+e193 = '''''' 
+
+e194 = '''''' 
+
+e195 = '''''' 
+
+e196 = '''''' 
+
+e197 = '''''' 
+
+e198 = '''''' 
+
+e199 = '''''' 
+
+e200 = '''''' 
+
+e201 = '''''' 
+
+e202 = '''''' 
+
+e203 = '''''' 
+
+e204 = '''''' 
+
+e205 = '''''' 
+
+e206 = '''''' 
+
+e207 = '''''' 
+
+e208 = '''''' 
+
+e209 = '''''' 
+
+e210 = '''''' 
+
+e211 = '''''' 
+
+e212 = '''''' 
+
+e213 = '''''' 
+
+e214 = '''''' 
+
+e215 = '''''' 
+
+e216 = '''''' 
+
+e217 = '''''' 
+
+e218 = '''''' 
+
+e219 = '''''' 
+
+e220 = '''''' 
+
+e221 = '''''' 
+
+e222 = '''''' 
+
+e223 = '''''' 
+
+e224 = '''''' 
+
+e225 = '''''' 
+
+e226 = '''''' 
+
+e227 = '''''' 
+
+e228 = '''''' 
+
+e229 = '''''' 
+
+e230 = '''''' 
+
+e231 = '''''' 
+
+e232 = '''''' 
+
+e233 = '''The driver flicks his cigarette into the gutter and steps into the 
+motor coach. Its engine coughs into life. You board, grateful that 
+you will be the only passenger for the initial part of your trip at 
+least. With mixed emotions, you watch from the window as the 
+tired avenues of your old home slip behind you, receding into 
+the distance. For a few minutes, you can still see the church spire 
+over the brow of a low hill. Then the road dips and it, too, is gone.
+Arkham is your new home. You will travel there, and make 
+a new start.
+'''
+
+e234 = '''''' 
+
+e235 = '''''' 
+
+e236 = '''''' 
+
+e237 = '''''' 
+
+e238 = '''''' 
+
+e239 = '''''' 
+
+e240 = '''''' 
+
+e241 = '''''' 
+
+e242 = '''''' 
+
+e243 = '''''' 
+
+e244 = '''''' 
+
+e245 = '''''' 
+
+e246 = '''''' 
+
+e247 = '''''' 
+
+e248 = '''''' 
+
+e249 = '''''' 
+
+e250 = '''''' 
+
+e251 = '''''' 
+
+e252 = '''''' 
+
+e253 = '''''' 
+
+e254 = '''''' 
+
+e255 = '''''' 
+
+e256 = '''''' 
+
+e257 = '''''' 
+
+e258 = '''''' 
+
+e259 = '''''' 
+
+e260 = '''''' 
+
+e261 = '''''' 
 
 e262 = '''You struggle with the huge man. His fists come out of the
 darkness like hammers.
@@ -102,6 +632,36 @@ otherwise this will be the first of many long bus trips.
 “Mmm-hm.” The driver scratches the match and lights his
 cigarette. The end flares as he takes a draw. Then he exhales and
 gestures to the back of the coach. “Luggage rack’s up there.
+'''
+
+e264 = '''''' 
+
+e265 = '''''' 
+
+e266 = '''''' 
+
+e267 = '''''' 
+
+e268 = '''''' 
+
+e269 = '''''' 
+
+e270 = '''The swirling tongues of fire around you stop in mid-air. The 
+people around the Beacon freeze, their black painted faces 
+bleached and stripped, as a second sun opens in the air above 
+Emberhead. In an instant, the people, the village, the hill, all 
+are consumed; incinerated by impossible proximity to sheer 
+combustion, the essence of fire.
+Though your body is bound to the Beacon, your being is freed. 
+As a spark you race into space, catapulted through the vastness of 
+the void. The stars burn past you with incomprehensible velocity. 
+And then you are home.
+Forever you will dwell here, at Fomalhaut; where the flames 
+ripple and flow through immense spaces to the rhythms of 
+the universe; where planets themselves move and tilt through 
+unutterable wheels of fire, bound to the clockwork chaos of the 
+Living Flame.
+And among the flames you will dance.
 '''
 
 def showevent(ev):
